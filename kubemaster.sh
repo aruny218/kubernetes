da92cf040c7899ccf51552aa651fd57b546886d3
Your Name <you@example.com>
2018-12-09 07:17:57 +0000

    ss

--- kubemaster.sh
+++ kubemaster.sh
@@ -24,12 +24,12 @@ DockerCERepo
 ## Installing Docker
 yum install bind-utils http://mirror.centos.org/centos/7/extras/x86_64/Packages/container-selinux-2.74-1.el7.noarch.rpm -y &>/dev/null
 yum install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-ce-18.06.0.ce-3.el7.x86_64.rpm -y &>/dev/null
-#if [ $? -eq 0 ]; then
-#success "Installed Docker-CE Successfully"
-#else
-#error "Installing Docker-CE Failure"
-#exit 1
-#fi
+# if [ $? -eq 0 ]; then
+# success "Installed Docker-CE Successfully"
+# else
+# error "Installing Docker-CE Failure"
+# exit 1
+# fi
 
 ## Starting Docker Service
 systemctl enable docker &>/dev/null
