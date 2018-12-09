e213ffcc648c3f00bc9dcb0c0854e730c6f4a5c2
Arun <arun.y218@gmail.com>
2018-12-09 08:33:15 +0000

    ss

--- kubenode.sh
+++ kubenode.sh
@@ -23,7 +23,7 @@ DockerCERepo
 
 ## Installing Docker
 yum install bind-utils http://mirror.centos.org/centos/7/extras/x86_64/Packages/container-selinux-2.74-1.el7.noarch.rpm -y &>/dev/null
-yum install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-ce-18.06.0.ce-3.el7.x86_64.rpm -y &>/dev/null
+yum install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-ce-17.03.3.ce-1.el7.x86_64.rpm -y &>/dev/null
 
 if [ $? -eq 0 ]; then
 success "Installed Docker-CE Successfully"
